8d1585cc838e42bc8a4e56d1eab24203e2b08dbf
xtm <2083933996@qq.com>
2026-03-28 19:51:25 +0800

    fix

--- dtm/init.go
+++ dtm/init.go
@@ -3,19 +3,31 @@ package driver
 import (
 	"fmt"
 	"github.com/dtm-labs/dtmdriver"
+	"os"
 )
 
 func init() {
-	// dtm 服务注册
-	Address := "192.168.163.132:8500"
-	Scheme := "http"
-	dtmDriver := NewXtmDriver(Address, Scheme)
+	// 1. 优先从环境变量获取，方便 K8s 部署时通过 env 注入
+	// 在你的 dtm 部署 yaml 里添加 ENV: CONSUL_ADDRESS
+	address := os.Getenv("CONSUL_ADDRESS")
+	if address == "" {
+		// 2. 如果没有环境变量，使用 K8s 内部 Consul 的 Service 域名
+		address = "consul:8500"
+	}
+
+	scheme := os.Getenv("CONSUL_SCHEME")
+	if scheme == "" {
+		scheme = "http"
+	}
+
+	fmt.Printf("正在初始化 DTM 驱动，Consul 地址: %s", address)
+
+	dtmDriver := NewXtmDriver(address, scheme)
 	dtmdriver.Register(dtmDriver)
-	// 调用的Use方法激活驱动
+
 	err := dtmdriver.Use(DriverName)
 	if err != nil {
-		fmt.Println("激活DTM驱动失败: %v", err)
-		panic(err)
+		panic(fmt.Sprintf("激活DTM驱动失败: %v", err))
 	}
 	fmt.Println("激活DTM驱动成功")
 }
